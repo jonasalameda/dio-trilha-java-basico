5366aead2edc130060028867599c70dacc1db524
jonas <125613982+jonasalameda@users.noreply.github.com>
2025-03-12 18:46:16 -0400

    initial commit

---
+++ src/main/java/org/jonas/ContaTerminal.java
@@ -0,0 +1,13 @@
+package org.jonas;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        //todo: scanner
+
+        //exibir mensagens para usuario
+
+        //obter pela scanner os valores digitados no terminal
+
+        //exibir msg de conta criada
+    }
+}
